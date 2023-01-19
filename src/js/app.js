dbbeda5e71a4824efa5ec0b1e7eef542ae1b2c3a
김나예 <kimnaye@gimnayes-MacBook-Air.local>
2023-01-20 03:48:58 +0900

    feat : navbar 기능 구현 완료 #4

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+const btn = document.querySelector('.btn');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    btn.classList.toggle('active');
+});
