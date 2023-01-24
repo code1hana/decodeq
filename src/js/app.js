2b18863b5969115b1313c3d3dddc1bf7cc5fffca
김나예 <kimnaye@gimnayes-MacBook-Air.local>
2023-01-25 04:06:58 +0900

    feat : 강의 상세 페이지 전역 스타일링, 탭 메뉴, 아코디언 메뉴, right nav bar 구현

--- src/js/app.js
+++ src/js/app.js
@@ -23,3 +23,37 @@ $("label[for='creator']").click(function(){
 	grade.style.display = 'none';
     school.style.display = 'none';
 })
+
+//detail
+const allOpen = document.querySelector('.all-open');
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const units = document.querySelector('.accordion-units');
+
+allOpen.addEventListener("click", function(){
+    if(allOpen.innerText = '모두 펼치기'){
+        units.style.maxHeight = + units.scrollHeight + "px";
+        plus.style.display = 'none';
+        minus.style.display = 'block';
+        allOpen.innerText = "모두 접기";
+    }
+    else {
+        units.style.maxHeight = null;
+        plus.style.display = 'block';
+        minus.style.display = 'none';
+        allOpen.innerText = "모두 펼치기"
+    }
+});
+
+
+plus.addEventListener("click", function(){
+    units.style.maxHeight = + units.scrollHeight + "px";
+    plus.style.display = 'none';
+    minus.style.display = 'block';
+});
+
+minus.addEventListener("click", function(){
+    units.style.maxHeight = null;
+    plus.style.display = 'block';
+    minus.style.display = 'none';
+});
